a7e37aca7910968d8c0229fedaff64c6bdbd95a3
Markusleilions <148310689+Markusleilions@users.noreply.github.com>
2023-10-18 10:17:01 +0300

    loops 1

---
+++ loops.py
@@ -0,0 +1,8 @@
+###Šeit var redzēt kā strādā loops
+```py
+names =['Anna','Oskars','Dzenifere']
+ages = [16,14,17]
+
+for i in range (3):
+    print(f"Čau mani sauc {names[i]} un man ir {ages[i]} gadi")
+```
